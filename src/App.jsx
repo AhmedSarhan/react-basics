43f19e9194f536961cf156e6b554f25371c0478a
AhmedSarhan <ahmedsarhan1515@gmail.com>
2024-08-24 12:57:48 +0300

    lesson-7: useEffect end

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
+  useEffect(() => {
+    alert("count is starting");
+  }, []);
+
+  useEffect(() => {
+    console.log("count in increasing", count);
+    if (count === 10) {
+      alert("count is now 10");
+    }
+  }, [count]);
   return (
     <>
       <h1>Side Effects</h1>
