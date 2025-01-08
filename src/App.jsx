67ddc909cb845d5a316181c6b86b77a3b9ee985c
AhmedSarhan <ahmedsarhan1515@gmail.com>
2025-01-08 14:27:30 +0200

    lesson-4: jsx listing in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,60 @@
 import { useState, Fragment } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-
-const renderAgeMessage = (age) => {
-  if (age === 25) {
-    return <h3>Hurray! you lived quarter a decade!</h3>;
-  } else if (age < 25) {
-    return <h3>Not 25 yet</h3>;
-  }
-  return <h3>getting old</h3>;
-};
 
 const App = () => {
   const [count, setCount] = useState(0);
 
-  let person = { age: 27, name: "John Doe" };
-
-  // if (person.age === 25) {
-  //   return <h3>Hurray! you lived quarter a decade!</h3>;
-  // }
   return (
     <Fragment>
-      <div className="card">
+      {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+      </div> */}
+      <div className="list">
+        <div className="card">
+          <img
+            src="https://www.sonypictures.ca/sites/canada/files/2022-12/DP_6390172_SpiderMan_NoWayHome_2000x3000_EST_1333x2000_thumbnail-min-min.jpg"
+            alt="Spiderman"
+            width={200}
+            height={300}
+          />
+          <h3>Spider-man: No Way Home</h3>
+          <div className="length_release">
+            <p>2021</p>
+            <p>2h 28min</p>
+          </div>
+          <p>Action, Adventure, Fantasy</p>
+        </div>
+        <div className="card">
+          <img
+            src="https://i.redd.it/xe73a62eq0n81.jpg"
+            alt="The Batman"
+            width={200}
+            height={300}
+          />
+          <h3>The Batman</h3>
+          <div className="length_release">
+            <p>2022</p>
+            <p>2h 55min</p>
+          </div>
+          <p>Action, Crime, Drama</p>
+        </div>
+        <div className="card">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSu0yJ6jMU5CB7693rX-tgGuTwXwP6kFhc3Q&s"
+            alt="The Matrix Resurrections"
+            width={200}
+            height={300}
+          />
+          <h3>The Matrix Resurrections</h3>
+          <div className="length_release">
+            <p>2021</p>
+            <p>2h 28min</p>
+          </div>
+          <p>Action, Sci-Fi</p>
+        </div>
       </div>
-      <div>
-        Person Name is {person.name} and he is {person.age} years old
-      </div>
-      {renderAgeMessage(person.age)}
-      {/* {person.age === 25 ? (
-        <h3>Hurray! you lived quarter a decade!</h3>
-      ) : (
-        <h3>Not 25 anymore / yet</h3>
-      )} */}
     </Fragment>
   );
 };
