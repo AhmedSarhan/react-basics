04f79bed76cadf4b9b8da3a36df1f1f67d0ce511
AhmedSarhan <ahmedsarhan1515@gmail.com>
2025-01-08 14:38:08 +0200

    lesson 4: rendering lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,30 @@
 import { useState, Fragment } from "react";
 import "./App.css";
-
+const movies = [
+  {
+    image:
+      "https://www.sonypictures.ca/sites/canada/files/2022-12/DP_6390172_SpiderMan_NoWayHome_2000x3000_EST_1333x2000_thumbnail-min-min.jpg",
+    title: "Spider-man: No Way Home",
+    release: "2021",
+    length: "2h 28min",
+    genres: ["Action", "Adventure", "Fantasy"],
+  },
+  {
+    image: "https://i.redd.it/xe73a62eq0n81.jpg",
+    title: "The Batman",
+    release: "2022",
+    length: "2h 55min",
+    genres: ["Action", "Crime", "Drama"],
+  },
+  {
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSu0yJ6jMU5CB7693rX-tgGuTwXwP6kFhc3Q&s",
+    title: "The Matrix Resurrections",
+    release: "2021",
+    length: "2h 28min",
+    genres: ["Action", "Sci-Fi"],
+  },
+];
 const App = () => {
   const [count, setCount] = useState(0);
 
@@ -12,48 +36,17 @@ const App = () => {
         </button>
       </div> */}
       <div className="list">
-        <div className="card">
-          <img
-            src="https://www.sonypictures.ca/sites/canada/files/2022-12/DP_6390172_SpiderMan_NoWayHome_2000x3000_EST_1333x2000_thumbnail-min-min.jpg"
-            alt="Spiderman"
-            width={200}
-            height={300}
-          />
-          <h3>Spider-man: No Way Home</h3>
-          <div className="length_release">
-            <p>2021</p>
-            <p>2h 28min</p>
+        {movies.map((movie) => (
+          <div className="card">
+            <img src={movie.image} alt={movie.title} width={200} height={300} />
+            <h3>{movie.title}</h3>
+            <div className="length_release">
+              <p>{movie.release}</p>
+              <p>{movie.length}</p>
+            </div>
+            <p>{movie.genres.join(", ")}</p>
           </div>
-          <p>Action, Adventure, Fantasy</p>
-        </div>
-        <div className="card">
-          <img
-            src="https://i.redd.it/xe73a62eq0n81.jpg"
-            alt="The Batman"
-            width={200}
-            height={300}
-          />
-          <h3>The Batman</h3>
-          <div className="length_release">
-            <p>2022</p>
-            <p>2h 55min</p>
-          </div>
-          <p>Action, Crime, Drama</p>
-        </div>
-        <div className="card">
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSu0yJ6jMU5CB7693rX-tgGuTwXwP6kFhc3Q&s"
-            alt="The Matrix Resurrections"
-            width={200}
-            height={300}
-          />
-          <h3>The Matrix Resurrections</h3>
-          <div className="length_release">
-            <p>2021</p>
-            <p>2h 28min</p>
-          </div>
-          <p>Action, Sci-Fi</p>
-        </div>
+        ))}
       </div>
     </Fragment>
   );
