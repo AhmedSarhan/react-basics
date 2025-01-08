3d13c217e7a79f32d91d6bf255f58d2c0bdefd87
AhmedSarhan <ahmedsarhan1515@gmail.com>
2025-01-08 14:06:32 +0200

    lesson 3: jsx conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,25 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-function App() {
+const renderAgeMessage = (age) => {
+  if (age === 25) {
+    return <h3>Hurray! you lived quarter a decade!</h3>;
+  } else if (age < 25) {
+    return <h3>Not 25 yet</h3>;
+  }
+  return <h3>getting old</h3>;
+};
+
+const App = () => {
   const [count, setCount] = useState(0);
 
-  let person = { age: 20, name: "John Doe" };
+  let person = { age: 27, name: "John Doe" };
+
+  // if (person.age === 25) {
+  //   return <h3>Hurray! you lived quarter a decade!</h3>;
+  // }
   return (
     <Fragment>
-      {/* <div> */}
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -18,9 +30,14 @@ function App() {
       <div>
         Person Name is {person.name} and he is {person.age} years old
       </div>
-      {/* </div> */}
+      {renderAgeMessage(person.age)}
+      {/* {person.age === 25 ? (
+        <h3>Hurray! you lived quarter a decade!</h3>
+      ) : (
+        <h3>Not 25 anymore / yet</h3>
+      )} */}
     </Fragment>
   );
-}
+};
 
 export default App;
